d3906ffb21d216b88a8e23635b45fcb4e1fecfbf
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-19 19:18:04 +0000

    Creating the .get and .post routes for restaurants

---
+++ routes/restaurants.js
@@ -0,0 +1,10 @@
+/*
+ * All routes for restaurants are defined here
+ * Since this file is loaded in server.js into api/restaurants,
+ *   these routes are mounted onto /restaurants
+ */
+
+ // NOTE: UPDATE server.js AS WELL
+
+const express = require('express');
+const router  = express.Router();
