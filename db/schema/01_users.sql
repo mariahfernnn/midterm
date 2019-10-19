5b80acb212db176590afc76fb344b5db8436b492
Aiman <51346974+aimanaaw@users.noreply.github.com>
2019-10-19 14:08:45 -0400

    Made changes to schema
    * Made changes to the schema and seeds files. Added the INSERT SQL commands and updated the schema field types for some of the tables
    
    * Made changes to DB

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -15,18 +15,28 @@ CREATE TABLE users (
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  location VARCHAR(255) NOT NULL,
+  street_address VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
   cuisine VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
-  time_ordered_at TIMESTAMP NO UPDATE,
-  date_ordered_on DATE ON UPDATE,
+  date_time_ordered_created TIMESTAMP NOT NULL,
   order_is_complete BOOLEAN DEFAULT FALSE NOT NULL,
   total_amount NUMERIC(6,2) NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  price NUMERIC(5,2) NOT NULL,
 );
 
 CREATE TABLE order_items (
@@ -37,10 +47,4 @@ CREATE TABLE order_items (
   menu_items_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
 );
 
-CREATE TABLE menu_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
-  name VARCHAR(255) NOT NULL,
-  price NUMERIC(5,2) NOT NULL,
-);
 
