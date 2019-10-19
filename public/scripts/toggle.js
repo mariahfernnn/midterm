cb89bf63d89c60b9b9d8db374dd3b6ca3b72af32
Araxiah <cewoo98@hotmail.com>
2019-10-19 15:31:46 -0400

    toggle button for resturants works

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -1,6 +1,13 @@
-
+//toggle for cuisine (button 2)
 $(document).ready(function () {
   $('#Cuisine').click(function () {
     $('#food').toggle(1000);
+  });
+});
+
+//toggle for resturants (button 3)
+$(document).ready(function () {
+  $('#Resturants').click(function () {
+    $('.resturants').toggle(1000);
   });
 });
