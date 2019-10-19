099cde4870de4129834e864a61bcb491776722b9
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-19 19:59:19 +0000

    Creating .get and .post routes for orders

---
+++ routes/orders.js
@@ -0,0 +1,11 @@
+/*
+ * All routes for orders are defined here
+ * Since this file is loaded in server.js into api/orders,
+ *   these routes are mounted onto /orders
+ */
+
+ // NOTE: UPDATE server.js AS WELL
+
+const express = require('express');
+const router  = express.Router();
+
