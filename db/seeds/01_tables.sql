a087d256c39394b6f4a30d9aedcccabfab22d1e1
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 15:09:18 +0000

    Added customer_id to order_items

--- db/seeds/01_tables.sql
+++ db/seeds/01_tables.sql
@@ -2,14 +2,15 @@
 
 -- INSERT INTO users
 INSERT INTO users (id, name, is_restaurant_owner)
-VALUES('1', 'Michelle', false);
+VALUES('1', 'Michelle', false),
+('2', 'Salvatore', true);
 
 -- INSERT INTO restaurants
-INSERT INTO restaurants (id, name, street_address, city, province, post_code, country, cuisine)
-VALUES ('1', 'Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian');
+INSERT INTO restaurants (id, name, street_address, city, province, post_code, country, cuisine, owner_id)
+VALUES ('1', 'Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian', '2');
 
 -- INSERT INTO orders
-INSERT INTO orders (id, date_time_ordered_created, order_is_complete, total_amount, restaurant_id, user_id)
+INSERT INTO orders (id, date_time_ordered_created, order_is_complete, total_amount, restaurant_id, customer_id)
 VALUES ('1', CURRENT_TIMESTAMP, false, '55', '1', '1');
 
 -- INSERT INTO menu_items
@@ -19,8 +20,8 @@ VALUES ('1', '1', 'Piemonte', 'flor di latte, truffle crema, roasted garlic, mix
 ('3', '1', 'Linguine al Pomodoro', 'yellow tomato pasta, cherry tomatoes, parmigiana, basil' '19');
 
 -- INSERT INTO order_items
-INSERT INTO order_items (id, quantity, restaurant_id, order_id, menu_items_id)
-VALUES ('1', '3', '1', '1', '3'),
-('1', '3', '1', '1', '4'),
-('1', '3', '1', '1', '5');
+INSERT INTO order_items (id, quantity, customer_id, restaurant_id, order_id, menu_items_id)
+VALUES ('1', '3', '1', '1', '1', '3'),
+('1', '3', '1', '1', '1', '4'),
+('1', '3', '1', '1', '1', '5');
 
