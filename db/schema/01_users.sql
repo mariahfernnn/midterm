194065005050a2cbc78596bd0ffcda22209ff757
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 14:49:17 +0000

    Added owner_id to restaurants table, changed user_id to customer_id in orders table, and added description to menu_items table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,17 +1,19 @@
--- Drop and recreate Users table (Example)
+-- DROP TABLE statements to rerun the file easily
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS restaurants CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 DROP TABLE IF EXISTS order_items CASCADE;
 DROP TABLE IF EXISTS menu_items CASCADE;
 
---  Updated tables - changed VARCHAR to TEXT
+--  Updated ALL tables - changed VARCHAR to TEXT
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
   is_restaurant_owner BOOLEAN DEFAULT FALSE NOT NULL
 );
 
+-- Included owner_id
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
@@ -20,7 +22,8 @@ CREATE TABLE restaurants (
   province TEXT NOT NULL,
   post_code TEXT NOT NULL,
   country TEXT NOT NULL,
-  cuisine TEXT NOT NULL
+  cuisine TEXT NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE orders (
@@ -29,13 +32,15 @@ CREATE TABLE orders (
   order_is_complete BOOLEAN DEFAULT FALSE NOT NULL,
   total_amount NUMERIC(6,2) NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 );
 
+-- Included description
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   name TEXT NOT NULL,
+  description TEXT NOT NULL,
   price NUMERIC(5,2) NOT NULL,
 );
 
