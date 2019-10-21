3a9509048d06d318d27d44876f7f27db2cfdfeb7
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 14:40:19 +0000

    Updated INSERT INTO for each table to include the primary key id values and condensed the values for menu_items and order_items

--- db/seeds/01_tables.sql
+++ db/seeds/01_tables.sql
@@ -1,30 +1,26 @@
--- Users table seeds here (Example)
--- Leave is_restaurant_owner blank
-INSERT INTO users (users.name, is_restaurant_owner)
-VALUES('Michelle', '');
+-- Midterm Food INSERT
 
-INSERT INTO restaurants (restaurants.name, street_address, city, province, post_code, country, cuisine)
-VALUES ('Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian');
+-- INSERT INTO users
+INSERT INTO users (id, name, is_restaurant_owner)
+VALUES('1', 'Michelle', false);
 
--- Leave the order_is_complete column blank
-INSERT INTO orders (date_time_ordered_created, order_is_complete, total_amount, restaurant_id, user_id)
-VALUES (CURRENT_TIMESTAMP, '', '55', '1', '1');
+-- INSERT INTO restaurants
+INSERT INTO restaurants (id, name, street_address, city, province, post_code, country, cuisine)
+VALUES ('1', 'Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian');
 
-INSERT INTO menu_items (restaurant_id, menu_items.name, description, price)
-VALUES ('1', 'Piemonte', 'flor di latte, truffle crema, roasted garlic, mix mushroom, tomato cheese', '21');
+-- INSERT INTO orders
+INSERT INTO orders (id, date_time_ordered_created, order_is_complete, total_amount, restaurant_id, user_id)
+VALUES ('1', CURRENT_TIMESTAMP, false, '55', '1', '1');
 
-INSERT INTO menu_items (restaurant_id, menu_items.name, description, price)
-VALUES ('1', 'Barbabietola', 'roasted beets, goat yoghurt, almond & pistashio dressing' '15');
+-- INSERT INTO menu_items
+INSERT INTO menu_items (id, restaurant_id, name, description, price)
+VALUES ('1', '1', 'Piemonte', 'flor di latte, truffle crema, roasted garlic, mix mushroom, tomato cheese', '21'),
+('2', '1', 'Barbabietola', 'roasted beets, goat yoghurt, almond & pistashio dressing' '15'),
+('3', '1', 'Linguine al Pomodoro', 'yellow tomato pasta, cherry tomatoes, parmigiana, basil' '19');
 
-INSERT INTO menu_items (restaurant_id, menu_items.name, description, price)
-VALUES ('1', 'Linguine al Pomodoro', 'yellow tomato pasta, cherry tomatoes, parmigiana, basil' '19');
-
-INSERT INTO order_items (quantity, restaurant_id, order_id, menu_items_id)
-VALUES ('3', '1', '1', '3');
-
-INSERT INTO order_items (quantity, restaurant_id, order_id, menu_items_id)
-VALUES ('3', '1', '1', '4');
-
-INSERT INTO order_items (quantity, restaurant_id, order_id, menu_items_id)
-VALUES ('3', '1', '1', '5');
+-- INSERT INTO order_items
+INSERT INTO order_items (id, quantity, restaurant_id, order_id, menu_items_id)
+VALUES ('1', '3', '1', '1', '3'),
+('1', '3', '1', '1', '4'),
+('1', '3', '1', '1', '5');
 
