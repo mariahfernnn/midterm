595367c62fcca5d10c2e71d94151345e18324100
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 15:08:18 +0000

    Added customer_id to order_items table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,11 +1,11 @@
--- DROP TABLE statements to rerun the file easily
+--DROP TABLE statements to rerun the file easily
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS restaurants CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 DROP TABLE IF EXISTS order_items CASCADE;
 DROP TABLE IF EXISTS menu_items CASCADE;
 
---  Updated ALL tables - changed VARCHAR to TEXT
+--Updated ALL tables - changed VARCHAR to TEXT
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE users (
   is_restaurant_owner BOOLEAN DEFAULT FALSE NOT NULL
 );
 
--- Included owner_id
+--Included owner_id
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE orders (
   customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 );
 
--- Included description
+--Included description
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
@@ -44,12 +44,14 @@ CREATE TABLE menu_items (
   price NUMERIC(5,2) NOT NULL,
 );
 
+--Included customer_id
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   quantity INTEGER NOT NULL,
+  customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  menu_items_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+  menu_items_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE
 );
 
 
