5954f32f1e5132935c30bc82344bc03d63f8bf93
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 22:03:07 +0000

    Created a checkout Routes file

---
+++ routes/checkout.js
@@ -0,0 +1,8 @@
+/*
+ * All routes for orders are defined here
+ * Since this file is loaded in server.js into api/checkout,
+ *   these routes are mounted onto /checkout
+ */
+
+const express = require('express');
+const checkoutRoutes  = express.Router();
