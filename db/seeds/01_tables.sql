3f84c2e0f36ff1cbc3d423707f57556a5af0b8da
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-21 19:21:59 +0000

    Added restaurant owners in users table and added restaurant info in restaurants table

--- db/seeds/01_tables.sql
+++ db/seeds/01_tables.sql
@@ -2,12 +2,16 @@
 
 -- INSERT INTO users
 INSERT INTO users (id, name, is_restaurant_owner)
-VALUES('1', 'Michelle', false),
-('2', 'Salvatore', true);
+VALUES('1', 'Salvatore Mele', true),
+('2', 'Steve Gonzalez', true),
+('3', 'Susur Lee', true);
 
 -- INSERT INTO restaurants
 INSERT INTO restaurants (id, name, street_address, city, province, post_code, country, cuisine, owner_id)
-VALUES ('1', 'Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian', '2');
+VALUES ('1', 'Oretta', '633 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Italian', '1'),
+('2', 'Baro', '485 King Street', 'Toronto', 'Ontario', 'M5V 1K4', 'Canada', 'Latin', '2'),
+('3', 'Lee', '601 King Street', 'Toronto', 'Ontario', 'M5V 1M5', 'Canada', 'Asian Fusion', '3');
+
 
 -- INSERT INTO orders
 INSERT INTO orders (id, date_time_ordered_created, order_is_complete, total_amount, restaurant_id, customer_id)
