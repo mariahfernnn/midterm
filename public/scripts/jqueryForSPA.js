de6f16092ea01b63b15a0c67e94b4ff4420074ce
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-25 04:09:46 +0000

    Commented out files

--- public/scripts/jqueryForSPA.js
+++ public/scripts/jqueryForSPA.js
@@ -1,25 +1,25 @@
  // Using jQuery's document ready function to ensure script loads after the document is ready
 
 // This function will create a list of restaurants
-console.log("loaded");
-$("document").ready(function () {
-  const createRestaurantList = function (restaurantObject) {
-    const HTMLmarkup = `
-  <div class="testingRestObj" data-restaurantId = ${restaurantObject.id}>
-                <div>
-                <span class="restaurant">${restaurantObject.name}</span>
-                </div>
-            </div>`
-    return HTMLmarkup;
-  }
+// console.log("loaded");
+// $("document").ready(function () {
+//   const createRestaurantList = function (restaurantObject) {
+//     const HTMLmarkup = `
+//   <div class="testingRestObj" data-restaurantId = ${restaurantObject.id}>
+//                 <div>
+//                 <span class="restaurant">${restaurantObject.name}</span>
+//                 </div>
+//             </div>`
+//     return HTMLmarkup;
+//   }
 
-  const renderRestaurants = function (restaurants) {
-    for (let eachRestaurant of restaurants) {
-      console.log('eachRestuarant', eachRestaurant);
-      const $restaurant = createRestaurantList(eachRestaurant);
-      $('#restaurantsList1').append($restaurant);
-    }
-  }
+//   const renderRestaurants = function (restaurants) {
+//     for (let eachRestaurant of restaurants) {
+//       console.log('eachRestuarant', eachRestaurant);
+//       const $restaurant = createRestaurantList(eachRestaurant);
+//       $('#restaurantsList1').append($restaurant);
+//     }
+//   }
   // Using jQuery's document ready function to ensure script loads after the document is ready
 
   // This function will create a list of menu items
@@ -38,59 +38,59 @@ $("document").ready(function () {
   //   return HTMLmarkup;
   // }
 
-  const renderMenu1 = function (menus) {
-    for (let eachMenu of menus) {
-      console.log('eachMenus', eachMenu);
-      const $menu = createMenuList(eachMenu);
-      $('#menuItems1').append($menu);
-    }
-  }
-  const renderMenu2 = function (menus) {
-    for (let eachMenu of menus) {
-      // console.log('eachMenus', eachMenu);
-      const $menu = createMenuList(eachMenu);
-      $('#menuItems2').append($menu);
-    }
-  }
-  const renderMenu3 = function (menus) {
-    for (let eachMenu of menus) {
-      // console.log('eachMenus', eachMenu);
-      const $menu = createMenuList(eachMenu);
-      $('#menuItems3').append($menu);
-    }
-  }
+  // const renderMenu1 = function (menus) {
+  //   for (let eachMenu of menus) {
+  //     console.log('eachMenus', eachMenu);
+  //     const $menu = createMenuList(eachMenu);
+  //     $('#menuItems1').append($menu);
+  //   }
+  // }
+  // const renderMenu2 = function (menus) {
+  //   for (let eachMenu of menus) {
+  //     // console.log('eachMenus', eachMenu);
+  //     const $menu = createMenuList(eachMenu);
+  //     $('#menuItems2').append($menu);
+  //   }
+  // }
+  // const renderMenu3 = function (menus) {
+  //   for (let eachMenu of menus) {
+  //     // console.log('eachMenus', eachMenu);
+  //     const $menu = createMenuList(eachMenu);
+  //     $('#menuItems3').append($menu);
+  //   }
+  // }
 
 
   // GET REQUEST for restaurants
-  const loadRestaurants = async () => {
-    try {
-      const response = await $.ajax({
-        url: `/api/restaurants`,
-        type: 'GET',
-        dataType: 'JSON'
-      })
-      renderRestaurants(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // const loadRestaurants = async () => {
+  //   try {
+  //     const response = await $.ajax({
+  //       url: `/api/restaurants`,
+  //       type: 'GET',
+  //       dataType: 'JSON'
+  //     })
+  //     renderRestaurants(response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
   // GET REQUEST for menus
-  const loadMenuItems = async () => {
-    try {
-      const response = await $.ajax({
-        url: `/api/menus`,
-        type: 'GET',
-        dataType: 'JSON'
-      })
-      const menus = JSON.parse(response);
-      renderMenu1(menus.menu1);
-      renderMenu2(menus.menu2);
-      renderMenu3(menus.menu3);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // const loadMenuItems = async () => {
+  //   try {
+  //     const response = await $.ajax({
+  //       url: `/api/menus`,
+  //       type: 'GET',
+  //       dataType: 'JSON'
+  //     })
+  //     const menus = JSON.parse(response);
+  //     renderMenu1(menus.menu1);
+  //     renderMenu2(menus.menu2);
+  //     renderMenu3(menus.menu3);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
   // Add menu items to the menuItemsArray in the order form
   // let menuItemsOrdered = [];
@@ -110,45 +110,45 @@ $("document").ready(function () {
 
 
   // THE POST REQUEST
-  const createOrder = async () => {
-    try {
-      const response = await $.ajax({
-        url: `/api/orders`,
-        type: 'POST',
-        data: 'JSON'
-      })
-      console.log('POSTING ORDER TO THE DATABASE', response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  // const createOrder = async () => {
+  //   try {
+  //     const response = await $.ajax({
+  //       url: `/api/orders`,
+  //       type: 'POST',
+  //       data: 'JSON'
+  //     })
+  //     console.log('POSTING ORDER TO THE DATABASE', response);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-  console.log("FUNC")
-  $('#orderForm').submit(function (e) {
-    e.preventDefault();
+  // console.log("FUNC")
+  // $('#orderForm').submit(function (e) {
+  //   e.preventDefault();
 
-    $.post(`/api/orders`, {
-      restaurantName: $('#restaurantName').val(),
-      userName: $('#userName').val(),
-      totalAmount: $('#totalAmount').val(),
-      menuItemsOrdered: menuItemsOrdered
-      }
-    )
-  })
+  //   $.post(`/api/orders`, {
+  //     restaurantName: $('#restaurantName').val(),
+  //     userName: $('#userName').val(),
+  //     totalAmount: $('#totalAmount').val(),
+  //     menuItemsOrdered: menuItemsOrdered
+  //     }
+  //   )
+  // })
 
-  $('#Resturants').click(function (e) {
-    e.preventDefault();
-  });
-  loadRestaurants();
-  loadMenuItems();
+  // $('#Resturants').click(function (e) {
+  //   e.preventDefault();
+  // });
+  // loadRestaurants();
+  // loadMenuItems();
 
   // Get items into cart function
-  const cart = [];
-  console.log("testests")
+  // const cart = [];
+  // console.log("testests")
 
-  $(".placeOrder").on('click  ', (evt) => {
-    evt.preventDefault();
+  // $(".placeOrder").on('click  ', (evt) => {
+  //   evt.preventDefault();
     // const item = {menuItemId: , quantity: price:};
     // debugger
-  })
-});
+//   })
+// });
--- public/scripts/quantityForMenuItem.js
+++ public/scripts/quantityForMenuItem.js
@@ -1,7 +1,7 @@
 // The script for the quantity in the menu
-$(document).ready(function() {
-  $("#quantity").keyup(function(e) {
-    const span = $(this).siblings('#quantity');
-    $(span).text(counterValue);
-  });
-});
+// $(document).ready(function() {
+//   $("#quantity").keyup(function(e) {
+//     const span = $(this).siblings('#quantity');
+//     $(span).text(counterValue);
+//   });
+// });
