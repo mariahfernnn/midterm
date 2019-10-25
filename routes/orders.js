0c81eb6401395da1cff5f81f83a3aca866c31f92
Aiman Azmie <aimanaaw@gmail.com>
2019-10-25 00:30:27 +0000

    Made changes to tweak the order route

--- routes/orders.js
+++ routes/orders.js
@@ -10,14 +10,14 @@ const queryFunction = require('../lib/query_functions');
 const sms = require("./twilio-sms");
 
 module.exports = function(db) {
-// Getting the orders for a particular restaurant
-  ordersRoutes.get("/", function(req, res) {
-    queryFunction.getOrderInfo(db, 'Oretta')
-    .then(rows => {
-      console.log("ORDER FUNCTION");
-      return res.json(rows);
-    })
-  });
+// // Getting the orders for a particular restaurant
+//   ordersRoutes.get("/", function(req, res) {
+//     queryFunction.getOrderInfo(db, 'Oretta')
+//     .then(rows => {
+//       console.log("ORDER FUNCTION");
+//       return res.json(rows);
+//     })
+//   });
 
 // Saving an order for a particular restaurant and order
 // Get request to get the restaurant id based on it's name
